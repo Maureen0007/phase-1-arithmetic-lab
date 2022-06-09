88ec475dc5ce92044d1af83bd36531c3b3106455
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-09 21:36:09 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 =  31
+let num2= 2
+const multiply = num1*num2;
+console.log(multiply)
 
-// Write your code here
+let random = 1
+Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 24
+let num4 = 5
+let mod = num3%num4;
+console.log(mod)
+
+let max= 20
+Math.max(2,15,20);
+console.log(max)
